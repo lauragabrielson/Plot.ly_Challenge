6aef45e94885a17efff959385638fd4cf0ff2305
lauragabrielson <lauramwinge@hotmail.com>
2021-04-17 15:26:17 -0500

    bar chart complete

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,57 @@
+// from Dom's instructional video
 
 console.log("app.js loaded");
 
-// from Dom's instructional 
+function drawBargraph(sampleId) {
+    console.log(`drawBargraph(${sampleId})`);
+    d3.json("samples.json").then(function(data) {
+        console.log(data);
+        var samples = data.samples;
+        var resultArray = samples.filter(s => s.id == sampleId);
+        var result = resultArray[0];
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+
+        yticks = otu_ids.slice(0, 10).map(otuId => `OTU ${otuId}`).reverse(); //TBD
+
+        var barData = {
+            x: sample_values.slice(0.10).reverse, //TBD
+            y: yticks,
+            type: "bar",
+            text: otu_labels.slice(0,10).reverse, //TBD
+            orientation: "h"
+        }
+
+        var barArray = [barData];
+        var barLayout = {
+            title: "Top 10 Bacterial Cultures Found",
+            margin: {t: 30, l: 150}
+        }
+
+        Plotly.newPlot("bar", barArray, barLayout);
+    });
+}
+
+function drawBubblechart(sampleId) {
+    console.log(`drawBubblechart(${sampleId})`);
+}
+
+function showMetadata(sampleId) {
+    console.log(`showMetadata(${sampleId})`);
+}
+
+// function drawGauge(sampleId) {}
+
+function optionChanged(newSampleId) {
+    console.log(`User selected ${newSampleId}`);
+
+    drawBargraph(newSampleId);
+    drawBubblechart(newSampleId);
+    showMetadata(newSampleId);
+}
+
+
 function initDashboard() {
     console.log("initDashboard()");
 
@@ -13,10 +63,15 @@ function initDashboard() {
         sampleNames.forEach(sampleId => {
             selector.append("option").text(sampleId).property("value", sampleId)
         });
+        var id = sampleNames[0]; 
+
+        // draw graphs and the metadata
+        drawBargraph(id);
+        drawBubblechart(id);
+        showMetadata(id);
+        // drawGauge(id);
     });
-    // update the bargraph
-    // update the bubblechart
-    // update the demographic info
+   
 }
 
 initDashboard();
